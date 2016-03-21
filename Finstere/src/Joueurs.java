65e343936f1f0b9a2206d5eed6dc3ceb7dfdaf73
Gab55 <gabrielgiroud@gmail.com>
2016-03-21 10:10:58 +0100

    ajouts attributs classe joueur

--- Finstere/src/Joueurs.java
+++ Finstere/src/Joueurs.java
@@ -13,6 +13,46 @@ import java.util.ArrayList;
  */
 public class Joueurs {
     private String pseudo ;
-    private ArrayList <Pions> pion  ;
+    private ArrayList <Pions> tabPion  ;
+    private boolean gagner ;
+    private int pionUtilisé;
+
+    public Joueurs(String pseudo, ArrayList<Pions> tabPion, boolean gagner, int pionUtilisé) {
+        this.pseudo = pseudo;
+        this.tabPion = tabPion;
+        this.gagner = gagner;
+        this.pionUtilisé = pionUtilisé;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public ArrayList<Pions> getTabPion() {
+        return tabPion;
+    }
+
+    public void setTabPion(ArrayList<Pions> tabPion) {
+        this.tabPion = tabPion;
+    }
+
+    public boolean isGagner() {
+        return gagner;
+    }
+
+    public void setGagner(boolean gagner) {
+        this.gagner = gagner;
+    }
     
+    public int getpionUtilisé(){
+        return pionUtilisé;
+    }
+    
+    public void setpionUtilisé(int pionUtilisé){
+        this.pionUtilisé= pionUtilisé;
+    }
 }

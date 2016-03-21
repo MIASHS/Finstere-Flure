ddcd35a6b78e407284beb941e099854ca96b5ae0
Gab55 <gabrielgiroud@gmail.com>
2016-03-21 09:55:36 +0100

    ajout class Pions et attributs dans joueurs

--- Finstere/src/Pions.java
+++ Finstere/src/Pions.java
@@ -1,6 +1,3 @@
-
-import java.util.ArrayList;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,8 +8,6 @@ import java.util.ArrayList;
  *
  * @author Gabriel
  */
-public class Joueurs {
-    private String pseudo ;
-    private ArrayList <Pions> pion  ;
+public class Pions {
     
 }

eaf1362ecc88c6902c4edd4e4b256d3b0c32a8c4
sbrogniart <sebastien.brogniart57570@gmail.com>
2016-03-21 10:30:25 +0100

    ajout de la classe PionJoueur + Getters Setters Constructeur et méthode Mourir

---
+++ Finstere/src/PionJoueur.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Seb
+ */
+public class PionJoueur {
+    
+   
+    //Attributs
+    public void setNumActuelle(int numActuelle) {
+        this.numActuelle = numActuelle;
+    }
+        private boolean mort; //booléen indiquant si un pion d'un joueur est mort
+        private boolean utilise;
+        private int[] tabNumDispo;
+        private int numActuelle;
+        
+    //Getters Setters
+        
+        public boolean isMort() {
+            return mort;
+        }
+
+        public void setMort(boolean mort) {
+            this.mort = mort;
+        }
+
+        public boolean isUtilise() {
+            return utilise;
+        }
+
+        public void setUtilise(boolean utilise) {
+            this.utilise = utilise;
+        }
+
+        public int[] getTabNumDispo() {
+            return tabNumDispo;
+        }
+
+        public void setTabNumDispo(int[] tabNumDispo) {
+            this.tabNumDispo = tabNumDispo;
+        }
+
+        public int getNumActuelle() {
+            return numActuelle;
+        }
+        
+    //Constructeur
+
+        public PionJoueur(boolean mort, boolean utilise, int[] tabNumDispo, int numActuelle) {
+            this.mort = mort;
+            this.utilise = utilise;
+            this.tabNumDispo =  tabNumDispo;
+            this.numActuelle = numActuelle;
+        }
+
+    //Méthodes
+        
+        //Méthode permettant de faire mourir un pion d'un joueur
+        public void mourir(boolean b){
+            this.setMort(b);
+        }
+    
+}

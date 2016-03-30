336cf266688c77baf7ec34d6c76161e95daa3ff4
Gab55 <gabrielgiroud@gmail.com>
2016-03-30 20:07:01 +0200

    correction

--- Finstere/src/Flaque.java
+++ Finstere/src/Flaque.java
@@ -9,30 +9,9 @@
  * @author Gabriel
  */
 public class Flaque extends Obstacle {
-    private int positionX;
-    private int positionY;
-
+    
     public Flaque(int positionX, int positionY) {
-        this.positionX = positionX;
-        this.positionY = positionY;
+        super(positionX, positionY);
     }
-
-    public int getPositionX() {
-        return positionX;
-    }
-
-    public void setPositionX(int positionX) {
-        this.positionX = positionX;
-    }
-
-    public int getPositionY() {
-        return positionY;
-    }
-
-    public void setPositionY(int positionY) {
-        this.positionY = positionY;
-    }
-    
-    
     
 }

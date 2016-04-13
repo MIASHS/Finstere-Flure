e1d797a8e527f9c08ac74fcf53cc56f9c0680ad7
0neiroi <valere.richier@gmail.com>
2016-04-13 17:39:48 +0200

    Modification de la classe monstres pour la prise en compte des cartes

--- Finstere/src/Monstre.java
+++ Finstere/src/Monstre.java
@@ -9,17 +9,18 @@
  * @author Gabriel
  */
 public class Monstre extends Pions{
-    private int x ;
+    private int x;
     private int y;
-    private int orientation;
-    private int[] listeCarte = new int[7];
-    private boolean changementO;
+    private int orientation; // 1=haut 3=droite 4=bas 2=gauche
+    private boolean chgmtOrientation=false;
+    private Paquet listeCarte = new Paquet();
+    int nbPionsTues=0;
+    
 
-    public Monstre(int x, int y, int orientation, boolean changementO) {
+    public Monstre(int x, int y, int orientation) {
         this.x = x;
         this.y = y;
         this.orientation = orientation;
-        this.changementO = changementO;
     }
 
     public int getX() {
@@ -46,23 +47,105 @@ public class Monstre extends Pions{
         this.orientation = orientation;
     }
 
-    public int[] getListeCarte() {
+    public Paquet getListeCarte() {
         return listeCarte;
     }
 
-    public void setListeCarte(int[] listeCarte) {
+    public void setListeCarte(Paquet listeCarte) {
         this.listeCarte = listeCarte;
     }
 
-    public boolean isChangementO() {
-        return changementO;
-    }
+    
+    // méthode
+    
+    public void deplacement(Cartes c){
+        if(c.getNumeroCarte()!=11&&c.getNumeroCarte()!=12){
+            int nbCasesRestantes = c.getNumeroCarte();
+            while(nbCasesRestantes!=0){
+                
+                
+                   this.regarder(this.getOrientation());
+                
 
-    public void setChangementO(boolean changementO) {
-        this.changementO = changementO;
+                switch(this.getOrientation()){
+                    case 1:
+                            this.setX(this.getX()+1);
+                            break;
+                    case 2:
+                            this.setY(this.getY()-1);
+                            break;
+                    case 3:
+                            this.setY(this.getY()+1);
+                            break;
+                    case 4:
+                            this.setX(this.getX()-1);
+                            break;
+                }
+                this.verifierCase();
+                nbCasesRestantes-=0;
+            }
+        }else{
+            int i;
+            if(c.getNumeroCarte()==11){
+                i=1;
+            }else{
+                i=2;
+            }
+            int nbCasesParcourues=0;
+            while(nbPionsTues!=i||nbCasesParcourues==20){
+                for(int j=1;j<4;j++){
+                   this.regarder(j);
+                }
+
+                switch(this.getOrientation()){
+                    case 1:
+                            this.setX(this.getX()+1);
+                            break;
+                    case 2:
+                            this.setY(this.getY()-1);
+                            break;
+                    case 3:
+                            this.setY(this.getY()+1);
+                            break;
+                    case 4:
+                            this.setX(this.getX()-1);
+                            break;
+                }
+                this.verifierCase();
+                nbCasesParcourues+=1;
+            }
+        }
+        
     }
     
+    public boolean changementO(){
+        return false;
+    }
+    public boolean regarder(int o){
+        switch(o){
+            case 1: 
+                    break;
+            case 2:
+                    break;
+            case 3:
+                    break;
+            case 4:
+                    break;
+        }
+        
+        return false;
+    }
+    public void seTeleporter(char c){
     
+    }
+    public void tuer(Pions p){
+    
+    }
+    
+    public void verifierCase(){
+    
+    }
+   
 
     
     

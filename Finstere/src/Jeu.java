9d5e643c0ff569a80bda338ae9f1880fd27fac70
Gab55 <gabrielgiroud@gmail.com>
2016-04-18 09:34:57 +0200

    changement

--- Finstere/src/Jeu.java
+++ Finstere/src/Jeu.java
@@ -34,19 +34,19 @@ public class Jeu {
        
        // mise en place des flaques
        this.flaque = new ArrayList<Flaque>();
-       Flaque flaque1 = new Flaque(100,90);
-       Flaque flaque2 = new Flaque (60,80);
+       Flaque flaque1 = new Flaque(8,10);
+       Flaque flaque2 = new Flaque (7,6);
        // ajout dans la liste
        this.flaque.add(flaque1);
        this.flaque.add(flaque2);
        
        // mise en place des bloques de pierres
        this.blocsPierre = new ArrayList<Pierre>();
-       Pierre p1 = new Pierre(108,100);
-       Pierre p2 = new Pierre(10,11);
-       Pierre p3 = new Pierre(30,55);
-       Pierre p4 = new Pierre(80,66);
-       Pierre p5 = new Pierre(120,100);
+       Pierre p1 = new Pierre(2,14);
+       Pierre p2 = new Pierre(1,6);
+       Pierre p3 = new Pierre(1,2);
+       Pierre p4 = new Pierre(9,7);
+       Pierre p5 = new Pierre(5,8);
        // ajout dans la liste
        this.blocsPierre.add(p1);
        this.blocsPierre.add(p2);
@@ -62,7 +62,7 @@ public class Jeu {
        
        j_list.add(joueur1);
        
-        Joueurs joueur2 = new Joueurs("Valére");
+        Joueurs joueur2 = new Joueurs("Valère");
        
        joueur2.ajouterPion(new PionJoueur(12,1));
        joueur2.ajouterPion(new PionJoueur(15,1));

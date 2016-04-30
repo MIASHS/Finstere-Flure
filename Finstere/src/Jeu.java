10846ea924846f37204ad29a746853f0ab98f536
0neiroi <valere.richier@gmail.com>
2016-04-30 19:20:49 +0200

    correction des erreurs code classe TestConsole,Plateau suite et amélioration esthétique de la carte de test

--- Finstere/src/Jeu.java
+++ Finstere/src/Jeu.java
@@ -67,13 +67,22 @@ public class Jeu {
        joueur2.ajouterPion(new PionJoueur(2));
        
        j_list.add(joueur2);
-       
+       for(int i = 0;i<monPlateau.getPlateau().size();i++){
+           System.out.println(monPlateau.getPlateau().get(i).getPioncase().getClass().getName());
+           System.out.println("X:  "+monPlateau.getPlateau().get(i).getPioncase().getX());
+           System.out.println("Y:  "+monPlateau.getPlateau().get(i).getPioncase().getY());
+           System.out.println("Y:  "+monPlateau.getPlateau().get(i).isOccupee());
+       }
+       System.out.println(monPlateau.getCase(monPlateau.getPlateau().get(0).getAbscisse(), monPlateau.getPlateau().get(0).getOrdonnee()).getPioncase().getClass().getName());
+       System.out.println("X=   "+monPlateau.getCase(monPlateau.getPlateau().get(0).getAbscisse(), monPlateau.getPlateau().get(0).getOrdonnee()).getAbscisse());
+       System.out.println("Y=   "+monPlateau.getCase(monPlateau.getPlateau().get(0).getAbscisse(), monPlateau.getPlateau().get(0).getOrdonnee()).getOrdonnee());
+       System.out.println("Y:  "+monPlateau.getPlateau().get(0).isOccupee());
    }
    
     public void placementFlaque(Plateau plateauVide,int i){
         Random ra = new Random();
         int X = ra.nextInt(15);
-        int Y = ra.nextInt(11);
+        int Y = (-1)*ra.nextInt(11);
         Cases c = plateauVide.getCase(X,Y);
         Cases c1;
         Cases c2;
@@ -82,8 +91,8 @@ public class Jeu {
             c1 = plateauVide.getCase(X+1,Y);
             c2 = plateauVide.getCase(X,Y-1);
             c3 = plateauVide.getCase(X+1,Y-1);
-        while (c.isOccupee()||c1.isOccupee()||c2.isOccupee()||c3.isOccupee() || (X > 8 && Y==0) || (X > 9 && Y==-1) || (X > 10 && Y==-2)|| (X > 11 && Y==-3)|| (X > 12 && Y>=-4) || (X == 0 && Y==0)){
-                Y = ra.nextInt(11);
+        while (c.isOccupee()||c1.isOccupee()||c2.isOccupee()||c3.isOccupee() || (X > 8 && Y==0) || (X > 9 && Y==-1) || (X > 10 && Y==-2)|| (X > 11 && Y==-3)|| (X > 12 && Y>=-4) || (X == 0 && Y==0) || (Y<-8) || (X == 0 && Y<-5) || (X == 1 && Y<-6) || (X == 2 && Y<-7) ){
+                Y = (-1)*ra.nextInt(11);
                 X = ra.nextInt(15);
                 c = plateauVide.getCase(X,Y);
                 c1 = plateauVide.getCase(X+1,Y);
@@ -95,7 +104,7 @@ public class Jeu {
             c2 = plateauVide.getCase(X+2,Y);
             c3 = plateauVide.getCase(X+3,Y);
         while (c.isOccupee() || c1.isOccupee()||c2.isOccupee()||c3.isOccupee() || ( Y==10) || (Y==9)|| (X == 0 && Y==0) || (X == 10 && Y==0)|| (X == 11 && Y==-1)|| (X == 12 && Y==-2)|| (X == 13 && Y==-3)|| (X == 14 && Y>=-4)|| (X == 15 && Y==-10)|| (X == 0 && Y==-6)|| (X == 1 && Y==-7)|| (X == 2 && Y==-8)){
-                Y = ra.nextInt(11);
+                Y = (-1)*ra.nextInt(11);
                 X = ra.nextInt(15);
                 c = plateauVide.getCase(X,Y);
                 c1 = plateauVide.getCase(X+1,Y);
@@ -112,10 +121,10 @@ public class Jeu {
     public void placementBlocPierre(Plateau plateauVide){
         Random ra = new Random();
         int placementX = ra.nextInt(15);
-        int placementY = ra.nextInt(11);
+        int placementY = (-1)*ra.nextInt(11);
         Cases c = plateauVide.getCase(placementX,placementY);
         while (c.isOccupee() || (placementX == 0 && placementY==1) || (placementX == 1 && placementY==1)|| (placementX == 0 && placementY==0) || (placementX == 1 && placementY==0)|| (placementX == 11 && placementY==15)|| (placementX == 11 && placementY==14)|| (placementX == 10 && placementY==14)|| (placementX == 10 && placementY==15)){
-                placementY = ra.nextInt(11);
+                placementY = (-1)*ra.nextInt(11);
                 placementX = ra.nextInt(15);
                 c = plateauVide.getCase(placementX,placementY);
         }
--- Finstere/src/TestConsole.java
+++ Finstere/src/TestConsole.java
@@ -26,12 +26,34 @@ public class TestConsole {
     }
 
     public static void testPlateau(Plateau p) {
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j > -11; j--) {
-                if (p.getCase(i,j).getPioncase() == null) {
-                    Outils.afficherTexte("[  ]");
-                } else {
-                    
+        Outils.afficherTexte("   ");
+        for(int k=0;k<16;k++){
+            if(k>9){
+                Outils.afficherTexte("  "+k+" ");
+            }else{
+                Outils.afficherTexte("  "+k+"  ");
+            }
+        }
+            
+        
+        Outils.afficherTexte("\n");
+        for (int j = 0; j > -11; j--) {
+            if(j==0){
+                System.out.print(" "+j+" ");
+            }else{
+                if(j==-10){
+                    System.out.print(j);
+                }else{
+                    System.out.print(j+" ");
+                }
+                
+            }
+            
+            
+            for (int i = 0; i < 16; i++) {
+                
+                
+                if (p.getCase(i,j).isOccupee() != false) {
                     switch (p.getCase(i, j).getPioncase().getClass().getName()) {
                         case "Pierre":
                             Outils.afficherTexte("[ P ]");
@@ -49,8 +71,17 @@ public class TestConsole {
                             Outils.afficherTexte("[ B ]");
                             break;
                     }
+                } else {
+                    if(!((i>=0&&j<=0&&i<=11&&j>=-6)||(i<=15&&j>=-10&&i>=4&&j<=-4)||(i<=14&&j>=-9&&i>=3&&j<=-3)||(i<=13&&j>=-8&&i>=2&&j<=-2)||(i<=12&&j>=-7&&i>=1&&j<=-1))){
+                        Outils.afficherTexte("[XXX]");
+                    }else{
+                         Outils.afficherTexte("[   ]");
+                    }
+                       
+                    
+                    }
                 }
-            }
+            
             Outils.afficherTexte("\n");
         }
     }
